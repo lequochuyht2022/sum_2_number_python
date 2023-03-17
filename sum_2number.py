d6cfd4d22ddada451c45ca1bd398b87cb58ac638
Le Quoc Huy <lequochuy.ht2022@gmail.com>
2023-03-17 19:58:32 +0700

    Add sum_2_number

---
+++ sum_2number.py
@@ -0,0 +1,10 @@
+#!/Users/lequochuy/opt/anaconda3/bin/python
+
+def sum_2number(a,b):
+	return a + b
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+c = sum_2number(a,b)
+print(f"Result is {c}")
